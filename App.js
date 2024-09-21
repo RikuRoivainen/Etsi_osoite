6334c442fc9f5c1f125e492d2054a3c3b72c5f0d
RikuRoivainen <bhd722@myy.haaga-helia.fi>
2024-09-21 12:53:27 +0300

    Tehtävää täydennetty

--- App.js
+++ App.js
@@ -84,7 +84,7 @@ const styles = StyleSheet.create({
   },
   inputContainer: {
     position: 'absolute',
-    top: 50, // Nostettu, mutta voit säätää tarpeen mukaan
+    top: 50, 
     width: '90%',
     zIndex: 1,
   },
@@ -97,7 +97,7 @@ const styles = StyleSheet.create({
   },
   map: {
     width: '100%',
-    height: '70%', // Vähennetty korkeutta, jotta syöte näkyy paremmin
-    marginTop: 100, // Lisätty tilaa kartan ylle
+    height: '70%', 
+    marginTop: 100, 
   },
 });
